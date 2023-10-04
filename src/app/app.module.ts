51f67b3a81818c0c92206aa5938a278bf32b2cb2
Josue Moreno <jossumike@gmail.com>
2023-10-03 20:05:33 -0600

    03-10-2023 JMMC

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatRadioModule } from '@angular/material/radio';
+
 import { AppComponent } from './app.component';
-import { SumasComponent } from './sumas/sumas.component';
+import { OperasBasComponent } from './operas-bas/operas-bas.component';
+import { SumaMultiComponent } from './suma-multi/suma-multi.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Sumas2Component } from './calculos/sumas2/sumas2.component';
+import { CinepolisFormComponent } from './calculos/cinepolis-form/cinepolis-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SumasComponent
+    OperasBasComponent,
+    SumaMultiComponent,
+    Sumas2Component,
+    CinepolisFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calculos/cinepolis-form/cinepolis-form.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cinepolis-form',
+  templateUrl: './cinepolis-form.component.html',
+  styleUrls: ['./cinepolis-form.component.css']
+})
+export class CinepolisFormComponent 
+{
+  nombre: string = "";
+  catComprador: number = 0;
+  catBoletos: number = 0;
+  valor: number = 0;
+  total: number = 0;
+  tarjeta: string = "";
+
+  Pagar(): void {
+    const catBoletos = this.catBoletos;
+    const catComprador = this.catComprador;
+    const tarjeta = this.tarjeta === 'Si';
+  
+    let descuento = 0;
+  
+    if (catBoletos >= 5) 
+    {
+      descuento = 0.15; 
+    } 
+    
+    else if (catBoletos >= 3 && catBoletos <= 5) 
+    {
+      descuento = 0.10; 
+    }
+  
+    const precioBoleto = 12.000;
+  
+    let valor = catBoletos * precioBoleto * catComprador; 
+    
+    if (descuento > 0) 
+    {
+      valor -= valor * descuento;
+    }
+  
+    if (tarjeta) 
+    {
+      const descuentoTarjeta = 0.10; 
+      valor -= valor * descuentoTarjeta;
+    }
+  
+    this.valor = valor;
+    this.total = valor;
+  }
+}
